708b41b87fc17265a28bcd15ebce49781d95bf8b
DenisLopushanskiy <Deees_batiy_cool@mail.ru>
2017-03-24 22:58:03 +0300

    Файлы Shiny. Паша, если есть возможность, удали папку "Shiny app"

---
+++ Shiny/ui.R
@@ -0,0 +1,31 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+# 
+# http://www.rstudio.com/shiny/
+#
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Investment portfolio choice"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarPanel(
+    selectInput("interval",
+                "Investment interval:",
+                c(30,60,90,180))
+  ),
+  sliderInput("VaR",
+              "How much are you ready to lose?",
+              min=1,
+              max=20,
+              value=20)
+  # Show a plot of the generated distribution
+  mainPanel(
+    tableOutput('firms')
+  )
+))
+
